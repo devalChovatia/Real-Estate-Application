76492efd9871009af26a960469181508c90428cc
devalChovatia <deval.chovatia@gmail.com>
2024-08-07 13:31:05 -0400

    complete update listing func

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -227,7 +227,9 @@ export default function Profile() {
 
           <div className="flex flex-col ">
             <button onClick={() =>handleDeleteListing(listing._id)} className="text-red-700">Delete</button>
-            <button className="text-green-700">Edit</button>
+            <Link to={`/updateListing/${listing._id}`}>
+              <button className="text-green-700">Edit</button>
+            </Link>
           </div>
 
         </div>

b128ccab899406ebf2212dddf15905e0acc5e1e5
devalChovatia <deval.chovatia@gmail.com>
2024-08-10 15:37:18 -0400

    bug fixes pt5

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, '/Client/dist')))
 app.get("*", (req, res) =>{
-    res.sendFile(path.join(__dirname, "/Client", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "Client", "dist", "index.html"))
 })
 
 
